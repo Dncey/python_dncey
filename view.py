30df7557aa9bef0596cc3cedbc410f9c42ebb9c6
Dncey <a1282463089@gmail.com>
2018-11-23 18:10:17 +0800

    创建视图login

--- view.py
+++ view.py
@@ -1,6 +1,10 @@
 from diango.http import HttpResopnse
-
+from django.shortcuts import redirect
 
 
 def index(request):
 	return HttpResponse('index')
+
+def login(request):
+
+	return redirect('/inedx')
